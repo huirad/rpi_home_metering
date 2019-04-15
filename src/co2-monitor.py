cf7e4455ebbe918ba9e6475e06a1fd2b6a76c782
huirad <huirad@github.com>
2019-04-15 19:47:27 +0200

    nicer

--- src/co2-monitor.py
+++ src/co2-monitor.py
@@ -9,6 +9,10 @@ References
 - Reverse Engineering done by Henry Ploetz     https://hackaday.io/project/5301/logs?sort=oldest
 - Derived Project from Michael Nosthoff        https://github.com/heinemml/CO2Meter
 - Derived Project from Thomas Reitmayr         https://gitlab.com/treitmayr/environment-monitor/tree/master/co2-mini
+
+Further References
+[ZG07]  http://www.zyaura.com/support/manual/pdf/ZyAura_CO2_Monitor_Carbon_Dioxide_ZG07%20series%20Module%20English%20user%20manual_1710.pdf
+
 '''
 
 import sys, fcntl, time, datetime
@@ -16,14 +20,14 @@ import sys, fcntl, time, datetime
 #we can use any key so - lets choose the one for which phase2 simply collapses
 key = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
 shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
-ctmp = [0x84,  0x47,  0x56,  0xD6,  0x07,  0x93,  0x93,  0x56]
+crot = [0x7C,  0xB9,  0xAA,  0x2A,  0xF9,  0x6D,  0x6D,  0xAA]
 
 
 def decrypt(key, data):
     phase1 = [data[i] for i in shuffle]
-    phase2 = phase1
+    phase2 = phase1     #xor'ing with 0 is identity
     phase3 = [((phase2[i] >> 3) | (phase2[(i-1+8)%8] << 5)) & 0xff for i in range(8)]
-    out = [(0x100 + phase3[i] - ctmp[i]) & 0xff for i in range(8)]
+    out = [(phase3[i] + crot[i]) & 0xff for i in range(8)]
     return out
 
 
@@ -48,15 +52,14 @@ if __name__ == "__main__":
             op = decrypted[0]
             val = decrypted[1] << 8 | decrypted[2]
             #print "%s\t%02X\t%04X" % (datetime.datetime.now().isoformat(), op, val)
-            #TODO CHECk RH reading? -> see http://www.zyaura.com/support/manual/pdf/ZyAura_CO2_Monitor_Carbon_Dioxide_ZG07%20series%20Module%20English%20user%20manual_1710.pdf
-            if op == 0x41:
-                rh = val/100.0
+            if op == 0x41:      #RH reading? -> see [ZG07]
+                rh = val/100.0  #apparently always 0
             if op == 0x42:
                 temp = val/16.0-273.15
             if op == 0x50:
                 co2 = val
-            if co2 and temp and rh:
-                print "%s\t%2.2f\t%4i\t%2.2f" % (datetime.datetime.now().isoformat(), temp, co2, rh)
+            if co2 is not None and temp is not None:
+                print "%s\t%2.2f\t%4i" % (datetime.datetime.now().isoformat(), temp, co2)
                 co2 = None
                 temp = None
                 rh = None
